8d6e158c47bb8c3fb8b450e45d51d32dfd08ecab
DSLKillerBR <breno.oliveira@hotmail.com.br>
2021-03-01 12:36:31 -0300

    fim da aula DIO

---
+++ js/explicacaojs.js
@@ -0,0 +1,65 @@
+/* É possível colocar as funções Javascript dentro do arquivo HTML,
+ou criar um arquivo .js com as funções */
+//var nome = "Breno Pinheiro de Oliveira"; //também é possível criar variáveis
+/* o tipo de varoiável foi identificado automaticamente pelo uso das aspas.
+quando se usa aspas, automaticamente é detectado ser uma string. */
+
+function soma(n1, n2){ //função de soma entre 2 números
+    return n1 + n2
+}
+function validaIdade (idade){
+    if (idade >= 18){
+     validar = true //se maior ou igual a 18, validar é true
+    } else{
+        validar = false //se não, é false
+    }
+    return validar //imprime a variável validar
+}
+var idade = prompt("Qual é a sua idade")
+console.log(validaIdade(idade))
+//alert (soma (5,10))
+
+/*var d = new Date () // puxa a data e a hora atual
+alert (d.getMinutes()) // mostra um pop-up com o minuto da hora atual
+/* var idade = prompt ("Qual é a sua idade?"); // abre um pop-up solicitando dados com a frase escrita dentro do parênteses, que serão gravados na variável "idade"
+var idade2 = 20;
+var lista = ["maçã", "pêra", "laranja"]
+var frase = "Eu amo a Isabela mais do que ela me ama"
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome : "uva", cor: "roxa"}] // é um dicionário de um objeto json
+// alert("pop-up da página do " + nome + ", que tem " + idade + " anos.");
+// alert (idade + idade2)  Resultado = abre um pop-up escrito "40"
+console.log (nome) //quando usamos esse comando, é impresso o que estiver
+console.log (idade) // dentro do parêntese no console do navegador (Inspecionar)
+console.log (frase.replace ("amo", "amo muito mais")) /* dessa forma, eu troquei
+"amo" para "amo muito mais"*/
+// console.log( frase.toUpperCase()) - a variável frase é impressa em caixa alta
+//console.log(frase.toLowerCase()) - a variável frase é impressa em minúsculo
+//alert(frase.replace ("amo", "amo muito mais")) - Também é possível usa essa função no alert
+/*lista.push("uva") // inseriu "uva" à lista
+lista.pop() // removeu "uva" da lista
+console.log(lista) //imprimiu a lista toda em forma de array
+console.log(lista[1]) // imprimiu só o item 1, lembrando que o 0 é um item.
+console.log(lista.length) //imprime o tamanho da lista
+console.log(lista.reverse()) //imprime a lista na ordem invertida
+console.log(lista.toString()) //imprime em forma de string
+console.log(lista[0]) 
+console.log(lista.toString()[0]) //imprime a primeira letra
+console.log(lista.join(" a ")) /* coloca o que estiver dentro do parênteses
+entre os elementos da lista */
+/*console.log (frutas) // imprime o dicionário frutas
+//console.log(fruta.nome) //imprime o que for "Nome" no dicionário
+//console.log(fruta.cor) //imprime o que for "cor" no dicionário
+//console.log(frutas [1].nome) // imprime "uva"
+/*if (idade > 18){
+    alert ("maior de idade")
+} else {
+    alert ("Menor de idade")
+}
+
+while (count <= 5){
+    console.log (count)
+    count = count + 1
+} //imprime o "count" até ele ser igual a 5
+for (count=0; count <=5; count++){ // declaramos o count
+    console.log(count)
+} //imprime o "count" até ele ser igual a 5*/
--- js/main.js
+++ js/main.js
@@ -1,50 +1,31 @@
-/* É possível colocar as funções Javascript dentro do arquivo HTML,
-ou criar um arquivo .js com as funções */
-//var nome = "Breno Pinheiro de Oliveira"; //também é possível criar variáveis
-/* o tipo de varoiável foi identificado automaticamente pelo uso das aspas.
-quando se usa aspas, automaticamente é detectado ser uma string. */
+function botao(){
+   // document.getElementById("agradecimento") - é possível pegar um elemento pela id no javascript
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!"// dessa forma, injetamos um texto no "agradecimento"
+   
+    // document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!" - é possível adicionar outras coisas além de um texto simples, como um texto em negrito, itálico, números, etc.
+   // console.log(document.getElementById("agradecimento"))
+   // alert("Obrigado por clicar")
+}
+function redirecionar(){
+    //window.open("https://www.cruzeiro.com.br/") //abre o link em uma nova página
+    window.location.href = "https://www.cruzeiro.com.br/" //abre o link na mesma página
 
-var d = new Date () // puxa a data e a hora atual
-alert (d.getMinutes()) // mostra um pop-up com o minuto da hora atual
-/* var idade = prompt ("Qual é a sua idade?"); // abre um pop-up solicitando dados com a frase escrita dentro do parênteses, que serão gravados na variável "idade"
-var idade2 = 20;
-var lista = ["maçã", "pêra", "laranja"]
-var frase = "Eu amo a Isabela mais do que ela me ama"
-var frutas = [{nome: "maçã", cor: "vermelha"}, {nome : "uva", cor: "roxa"}] // é um dicionário de um objeto json
-// alert("pop-up da página do " + nome + ", que tem " + idade + " anos.");
-// alert (idade + idade2)  Resultado = abre um pop-up escrito "40"
-console.log (nome) //quando usamos esse comando, é impresso o que estiver
-console.log (idade) // dentro do parêntese no console do navegador (Inspecionar)
-console.log (frase.replace ("amo", "amo muito mais")) /* dessa forma, eu troquei
-"amo" para "amo muito mais"*/
-// console.log( frase.toUpperCase()) - a variável frase é impressa em caixa alta
-//console.log(frase.toLowerCase()) - a variável frase é impressa em minúsculo
-//alert(frase.replace ("amo", "amo muito mais")) - Também é possível usa essa função no alert
-/*lista.push("uva") // inseriu "uva" à lista
-lista.pop() // removeu "uva" da lista
-console.log(lista) //imprimiu a lista toda em forma de array
-console.log(lista[1]) // imprimiu só o item 1, lembrando que o 0 é um item.
-console.log(lista.length) //imprime o tamanho da lista
-console.log(lista.reverse()) //imprime a lista na ordem invertida
-console.log(lista.toString()) //imprime em forma de string
-console.log(lista[0]) 
-console.log(lista.toString()[0]) //imprime a primeira letra
-console.log(lista.join(" a ")) /* coloca o que estiver dentro do parênteses
-entre os elementos da lista */
-/*console.log (frutas) // imprime o dicionário frutas
-//console.log(fruta.nome) //imprime o que for "Nome" no dicionário
-//console.log(fruta.cor) //imprime o que for "cor" no dicionário
-//console.log(frutas [1].nome) // imprime "uva"
-/*if (idade > 18){
-    alert ("maior de idade")
-} else {
-    alert ("Menor de idade")
+}
+function trocar(elemento){ //colocar elemento quando usar o (this)
+    //alert("Mouse passado com sucesso")
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    elemento.innerHTML = "Obrigado por passar o mouse"
+}
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+    elemento.innerHTML = "Passe o mouse aqui" // usamos o "elemento" ao invés do "document"
+    
+}
+function load(){
+    alert("Página carregada")
+}
+function funcaoChange(elemento){
+    console.log(elemento.value) // o .value, le o valor da opção
 }
 
-while (count <= 5){
-    console.log (count)
-    count = count + 1
-} //imprime o "count" até ele ser igual a 5
-for (count=0; count <=5; count++){ // declaramos o count
-    console.log(count)
-} //imprime o "count" até ele ser igual a 5*/
+
